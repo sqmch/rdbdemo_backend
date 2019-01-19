f82f28f6220334b54500231c22b834c107f39140
sqaku <sqmchh@gmail.com>
2019-01-19 11:53:09 +0000

    Remove typo from reddit instance declaration

--- datagrabber.py
+++ datagrabber.py
@@ -8,7 +8,7 @@ import db
 from textblob import TextBlob
 
 
-reddit = self.reddit = praw.Reddit(
+reddit = praw.Reddit(
     client_id=os.environ.get("CLIENT_ID"),
     client_secret=os.environ.get("CLIENT_SECRET"),
     password=os.environ.get("PASSWORD"),
